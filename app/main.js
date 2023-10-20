71c01ee01e3f0c65bfb7ea4f5fe9ff6fc95955ae
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2023-10-20 11:04:04 -0400

    pass 3rd stage | attempt #7 | Debugging

--- app/main.js
+++ app/main.js
@@ -11,11 +11,11 @@ const server = net.createServer((socket) => {
     // socket.write(`${STATUS_LINE} ${CRLF}${CRLF}`);
     const parts = req.toString().split("\r\n");
     const startLine = parts[0];
-
+    console.log("received:", parts, startLine);
     const [method, path, version] = startLine.split(" ");
     console.log({ method, path, version });
 
-    if (path[-1] === "/") {
+    if (path === "/") {
       socket.write("200 OK");
     } else {
       socket.write("404 NOT FOUND");
