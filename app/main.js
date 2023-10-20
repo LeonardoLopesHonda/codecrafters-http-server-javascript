eb012e4432be9cfb314fd83be628b61dadee74b0
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2023-10-20 10:39:05 -0400

    pass 3nd stage 1st attempt

--- app/main.js
+++ app/main.js
@@ -5,10 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    const STATUS_LINE = "HTTP/1.1 200 OK";
+  socket.on("data", (req) => {
+    // const STATUS_LINE = "HTTP/1.1 200 OK";
     const CRLF = "\r\n";
-    socket.write(`${STATUS_LINE} ${CRLF}${CRLF}`);
+    // socket.write(`${STATUS_LINE} ${CRLF}${CRLF}`);
+    const parts = req.split(CRLF);
+    const path = parts[1];
+
+    if (path[0] === "/") {
+      socket.write("200 OK");
+    } else {
+      socket.write("404 NOT FOUND");
+    }
+
     socket.end();
   });
   socket.on("close", () => {
